fe6174aee55721afcbbae13c2dbd2eb2b22d5c8b
foxy pirate cove / Fnaf <108185011+foxypiratecove37350@users.noreply.github.com>
2024-10-06 00:50:32 +0000

    Search result page's title

--- src/SearchPage.vue
+++ src/SearchPage.vue
@@ -1,10 +1,14 @@
 <script setup>
 
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 const query = computed(() => {
     return useRoute().query.q
+})
+
+onMounted(() => {
+    document.title = `"${query.value}" - ` + document.title
 })
 
 </script>
